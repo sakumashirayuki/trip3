b083a7b0e7a0916aca724fb3970eb40803c1bc8e
sakumashirayuki <2909549511@qq.com>
2022-03-28 17:01:38 +0800

    dva async

--- src/pages/users/model.ts
+++ src/pages/users/model.ts
@@ -1,22 +1,44 @@
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+];
+
 const model = {
-    namespace: 'users',
-    state: {},
-    reducers: {
-        getList(){
-            return 123;
-        }
+  namespace: 'users',
+  state: {},
+  reducers: {
+    getList(state: any, action: any) {
+      return { listData: action.payload };
     },
-    effects: {},
-    subscriptions: {
-        setup({dispatch, history}:{dispatch: any, history: any}){
-            return history.listen(({ pathname } : {pathname: string}) => {
-                if(pathname == '/users'){
-                    dispatch({
-                        type: 'getList'
-                    })
-                }
-            })
-        }
+  },
+  effects: {
+    *asyncGetData(action: any, effects: any): Generator {
+      const listData = yield Promise.resolve(data);
+      yield effects.put({ type: 'getList', payload: listData });
     },
+  },
+  subscriptions: {
+    setup({ dispatch, history }: { dispatch: any; history: any }) {
+      return history.listen(({ pathname }: { pathname: string }) => {
+        if (pathname == '/users') {
+          dispatch({
+            type: 'asyncGetData',
+          });
+        }
+      });
+    },
+  },
 };
 export default model;
